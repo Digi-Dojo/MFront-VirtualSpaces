b0582b1954a223d89e12fdf2eb475b2ccd41a733
Francesco <francesco.mazzini432@gmail.com>
2023-04-05 15:57:02 +0200

    Added OptionsSelector component for choosing among various input possible values, in our case to choose the type of Place
    Signed-off-by: Francesco <francesco.mazzini432@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
-import { NumericField } from './components/Fields';
+import { NumericField, OptionsSelector } from './components/Fields';
 import { useState } from 'react';
 
 function App() {
 
   const [startupId, setStartupId] = useState();
-
+  const [type, setType] = useState();
+ 
   return (
     <div className="App">
       <NumericField fieldTitle={"Startup Id"} numValue = {startupId} setNumValue = {setStartupId} />
+      <OptionsSelector fieldTitle={"Type"} 
+        options = {["Personal Desk", "Meeting", "Board"]}
+        selectedOption = {type}
+        setSelectedOption = {setType}
+      />
     </div>
   );
 }
--- src/components/Fields.jsx
+++ src/components/Fields.jsx
@@ -1,22 +1,44 @@
-import { TextField } from "@mui/material";
+import { TextField, FormControl, InputLabel, Select, MenuItem } from "@mui/material";
 
 const NumericField = ({fieldTitle, numValue, setNumValue}) => {
 
-    const isFieldValid = (fieldVal) => (fieldVal != '' && !isNaN(fieldVal))
+    const isFieldValid = (fieldVal) => (fieldVal !== '' && !isNaN(fieldVal))
 
     return (
-        <div>
-            <TextField
-                required
-                id="filled-required"
-                label={fieldTitle}
-                variant="filled"
-                onChange={(val) => setNumValue(val.target.value)}
-                error = { !isFieldValid(numValue) }
-            />
-        </div>
+        <TextField
+            id="numeric-field"
+            label={fieldTitle}
+            variant="filled"
+            onChange={(val) => setNumValue(val.target.value)}
+            error = { !isFieldValid(numValue) }
+            fullWidth
+        />
     )
 
 }
 
-export {NumericField};
+const OptionsSelector = ({fieldTitle, options, selectedOption, setSelectedOption}) => {
+
+    const handleChange = (val) => setSelectedOption(val.target.value);
+    const isFieldValid = (fieldVal) => (fieldVal !== undefined);
+    
+    return (
+        <FormControl fullWidth required error = { !isFieldValid(selectedOption)}>
+            <InputLabel>{fieldTitle}</InputLabel>
+            <Select
+                id="options-selector"
+                value={selectedOption}
+                label={fieldTitle}
+                onChange={handleChange}
+            >
+                {
+                    options.map(option => 
+                        <MenuItem value={option}>{option}</MenuItem>
+                    )
+                }
+            </Select>
+        </FormControl>
+    )
+}
+
+export {NumericField, OptionsSelector};
