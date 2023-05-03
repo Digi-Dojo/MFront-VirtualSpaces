a0dc7e185cbd2642b8868a2d56a64521208a7dae
João Daniel <jotaf.daniel@gmail.com>
2023-05-03 17:07:20 +0200

    port NoteFragment
    Signed-off-by: João Daniel <jotaf.daniel@gmail.com>

--- NoteFragment/src/root.component.js
+++ NoteFragment/src/root.component.js
@@ -1,3 +1,26 @@
-export default function Root(props) {
-  return <section>{props.name} is mounted!</section>;
+import { useState } from 'react';
+import { Button, TextField } from '@mui/material';
+import { BorderAllOutlined } from '@mui/icons-material';
+
+const NoteFragment = ({ note }) => {
+    
+    const [status, setStatus] = useState(true);
+
+    const changeStatus = () => {
+        setStatus(!status);
+    }
+
+    return (
+        <div key={note.id} style={{border: '2px', backgroundColor: '#f6f5aa', borderRadius: "5px", padding: 10}}>
+            <p>{note.text}</p>
+            <div>
+                <Button variant="outlined" onClick={changeStatus}
+                        title='REMOVE'>
+                    REMOVE
+                </Button>
+            </div>
+        </div>
+    )
 }
+
+export default NoteFragment;
