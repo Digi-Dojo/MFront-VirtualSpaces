8b324ca68e7a56d912b0208e3ffdb79c6d86636c
Francesco <francesco.mazzini432@gmail.com>
2023-06-06 09:43:40 +0200

    Added new fragment to represent notes list in a place
    Signed-off-by: Francesco <francesco.mazzini432@gmail.com>

---
+++ NotesInPlaceList/src/root.component.js
@@ -0,0 +1,34 @@
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import Place from "./components/Place";
+
+ const NotesInPlaceList = () => {
+
+  // const [placces]
+
+  const places = [{ startupId: 2, type: "BOARD"}, { startupId: 5, type: "MEETING ROOM"}, { startupId: 6, type: "PERSONAL DESK"}]
+    
+    return (
+        <Grid container>
+            <Box
+              sx={{
+                p: 2,
+                display: 'grid',
+                /* TODO:
+                    Deciding the place used by a UI element inside itself it's not so good, especially because
+                    this gridTemplateColumns makes it go out the borders of an external container. So this should
+                    be adjusted such that it takes 100% of the space and that could be externally reduced
+                 */
+                gridTemplateColumns: { md: '1fr 1fr 1fr 1fr' },
+                gap: 3,
+              }}
+            >
+              {places.map(place => place && <Place style={{padding: '5px'}} key={place.id} place={place}/>)}
+            </Box>
+        </Grid>
+    );
+       
+}
+
+export default NotesInPlaceList;
+
