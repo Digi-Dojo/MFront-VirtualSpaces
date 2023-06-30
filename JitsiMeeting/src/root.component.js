c1b87408668fd4573ff1c3823c2da33e50acbe00
Francesco <francesco.mazzini432@gmail.com>
2023-06-30 23:21:49 +0200

    Added new Microfrontend for JitsiMeetings
    Signed-off-by: Francesco <francesco.mazzini432@gmail.com>

---
+++ JitsiMeeting/src/root.component.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import JitsiComponent from './JitsiComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <JitsiComponent />
+    </div>
+  );
+}
+
+export default App;
